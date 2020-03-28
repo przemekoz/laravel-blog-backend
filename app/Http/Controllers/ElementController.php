14aae8c1e0334dd838fc74ab0ccab3820ae9b94a
Przemek Kozinski <przemekoz@o2.pl>
2020-03-28 09:47:58 +0100

    added : maping for Element endpoint responses : JSON:API

--- app/Http/Controllers/ElementController.php
+++ app/Http/Controllers/ElementController.php
@@ -5,21 +5,43 @@ namespace App\Http\Controllers;
 use App\Element;
 use Illuminate\Http\Request;
 
+
+
 class ElementController extends Controller
 {
+	
+	private function mapElement(Element $element) {
+		return ["id" => $element->id, "type" => "elements", "attributes" => ["title" => $element->title, "description" => $element->description]];
+	}
+	
+	private function mapElementWithTags(Element $element) {
+		
+		$relData = array();
+		foreach ($element->tags()->get() as $tag) {
+			array_push($relData, ["id" => $tag->id, "type" => "tags"]);
+		}
+		
+		return ["id" => $element->id, "type" => "elements", "attributes" => ["title" => $element->title, "description" => $element->description], "relationships" => ["tags" => ["data" => $relData] ]];
+	}
+	
     public function index()
     {
-        return Element::all();
+		$arr = array();
+		foreach (Element::all() as $elem) {
+			array_push($arr, $this->mapElement($elem));
+		}
+        //return Element::all();
+		return $arr;
     }
  
     public function show(Element $element)
     {
-        return $element;
+		return response()->json($this->mapElement($element), 200);
     }
 
     public function showWithTags(Element $element)
     {
-        return response()->json(["title" => $element->title, "description" => $element->description, "tags" => $element->tags()->get() ], 200);
+        return response()->json($this->mapElementWithTags($element), 200);
     }
 
     public function store(Request $request)
